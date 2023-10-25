23eb837df5324c8e0f9a145ee026fd9eb5ab0389
francisamanyo <amanyofrancis@yahoo.com>
2023-10-25 09:59:21 -0700

    all

--- 0x00-pagination/0-main.py
+++ 0x00-pagination/0-main.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 """
-Main file
+Contains definition of index_range helper function
 """
+from typing import Tuple
 
-index_range = __import__('0-simple_helper_function').index_range
 
-res = index_range(1, 7)
-print(type(res))
-print(res)
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Takes 2 integer arguments and returns a tuple of size two
+    containing the start and end index corresponding to the range of
+    indexes to return in a list for those pagination parameters
+    Args:
+        page (int): page number to return (pages are 1-indexed)
+        page_size (int): number of items per page
+    Return:
+        tuple(start_index, end_index)
+    """
+    start, end = 0, 0
+    for i in range(page):
+        start = end
+        end += page_size
 
-res = index_range(page=3, page_size=15)
-print(type(res))
-print(res)
+    return (start, end)
