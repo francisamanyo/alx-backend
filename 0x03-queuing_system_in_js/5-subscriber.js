c0c1d3bbc90f104e98366f695baf6059092ef761
root <amanyofrancis@yahoo.comgit config --global user.email amanyofrancis@yahoo.com>
2023-11-29 00:25:34 -0800

    all

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,22 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log(
+  'Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log('Message received on channel', channel, ':', message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
+});
